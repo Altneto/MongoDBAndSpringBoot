1c656ca682431080b08de00bc06409fbb2f7a55c
Altneto <neto1171995@gmail.com>
2022-09-12 16:48:53 -0300

    First commit

---
+++ src/main/java/com/altneto/springMongo/MongoDbAndSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.altneto.springMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbAndSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbAndSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/altneto/springMongo/MongoDbAndSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altneto.springMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbAndSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
