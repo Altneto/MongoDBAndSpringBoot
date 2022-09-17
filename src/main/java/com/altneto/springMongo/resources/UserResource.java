85e8133cf1f51276a66c9e19c0adcbcda3dc1774
Altneto <neto1171995@gmail.com>
2022-09-17 01:49:11 -0300

    find user by id and exception handling

--- src/main/java/com/altneto/springMongo/resources/UserResource.java
+++ src/main/java/com/altneto/springMongo/resources/UserResource.java
@@ -1,15 +1,14 @@
 package com.altneto.springMongo.resources;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.altneto.springMongo.domain.User;
 import com.altneto.springMongo.dto.UserDto;
 import com.altneto.springMongo.services.UserService;
 
@@ -22,8 +21,13 @@ public class UserResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<UserDto>> findAll() {
-		List<User> list = userService.findAll();
-		List<UserDto> dtos = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
+		List<UserDto> dtos = userService.findAll();
 		return ResponseEntity.ok().body(dtos);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDto> FindById(@PathVariable String id) {
+		UserDto dto = userService.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}
 }
--- src/main/java/com/altneto/springMongo/services/UserService.java
+++ src/main/java/com/altneto/springMongo/services/UserService.java
@@ -1,12 +1,16 @@
 package com.altneto.springMongo.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.altneto.springMongo.domain.User;
+import com.altneto.springMongo.dto.UserDto;
 import com.altneto.springMongo.repositories.UserRepository;
+import com.altneto.springMongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -14,7 +18,19 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 
-	public List<User> findAll() {
-		return userRepository.findAll();		
+	public List<UserDto> findAll() {
+		List<User> users = userRepository.findAll();
+		List<UserDto> dtos = users.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
+		return dtos;
+	}
+	
+	public UserDto findById(String id) {
+		Optional<User> user = userRepository.findById(id);
+		
+		if (!user.isPresent()) {
+			throw new ObjectNotFoundException("Usuário não encontrado");
+		}
+		
+		return new UserDto(user.get());	
 	}
 }
