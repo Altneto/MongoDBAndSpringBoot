f2fa421487b0ba37376d8289f7348db2a1996ed4
Altneto <neto1171995@gmail.com>
2022-09-22 11:21:15 -0300

    Find by title method

--- src/main/java/com/altneto/springMongo/resources/PostResource.java
+++ src/main/java/com/altneto/springMongo/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.altneto.springMongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.altneto.springMongo.domain.Post;
@@ -21,5 +24,11 @@ public class PostResource {
 	public ResponseEntity<Post> FindById(@PathVariable String id) {
 		Post post = postService.findById(id);
 		return ResponseEntity.ok().body(post);
+	}
+	
+	@RequestMapping(value = "/title", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> FindByTitle(@RequestParam(value = "text") String text) {
+		List<Post> posts = postService.findByTilte(text);
+		return ResponseEntity.ok().body(posts);
 	}
 }
--- src/main/java/com/altneto/springMongo/services/PostService.java
+++ src/main/java/com/altneto/springMongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.altneto.springMongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,11 @@ public class PostService {
 		}
 		
 		return post.get();
+	}
+	
+	public List<Post> findByTilte(String text) {
+		List<Post> posts = postRepository.findByTitleContaining(text);
+		
+		return posts;
 	}
 }
