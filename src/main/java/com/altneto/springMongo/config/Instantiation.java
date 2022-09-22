d0fc8e2acbedcee11c77931cb98bcc1e33535163
Altneto <neto1171995@gmail.com>
2022-09-22 10:36:04 -0300

    User with reference to post.

--- src/main/java/com/altneto/springMongo/config/Instantiation.java
+++ src/main/java/com/altneto/springMongo/config/Instantiation.java
@@ -35,7 +35,12 @@ public class Instantiation implements CommandLineRunner {
 				Post post2 = new Post(null, LocalDate.now(), "Bom dia", "Acordei feliz hoje!", new AuthorDto(maria));
 				
 				postRepository.saveAll(Arrays.asList(post1, post2));
+				
+				maria.getPosts().addAll(Arrays.asList(post1, post2));
+				userRepository.save(maria);
 			}
+			
+			
 		}
 	}
 }
