89a42dc000f501cf11a4b5f9ba835888aa18ca14
Altneto <neto1171995@gmail.com>
2022-09-22 10:53:24 -0300

    End point to find post by id.

---
+++ src/main/java/com/altneto/springMongo/resources/PostResource.java
@@ -0,0 +1,25 @@
+package com.altneto.springMongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.altneto.springMongo.domain.Post;
+import com.altneto.springMongo.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+	
+	@Autowired
+	private PostService postService;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> FindById(@PathVariable String id) {
+		Post post = postService.findById(id);
+		return ResponseEntity.ok().body(post);
+	}
+}
---
+++ src/main/java/com/altneto/springMongo/services/PostService.java
@@ -0,0 +1,27 @@
+package com.altneto.springMongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.altneto.springMongo.domain.Post;
+import com.altneto.springMongo.repositories.PostRepository;
+import com.altneto.springMongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		Optional<Post> post = postRepository.findById(id);
+		
+		if (!post.isPresent()) {
+			throw new ObjectNotFoundException("Post não encontrado");
+		}
+		
+		return post.get();
+	}
+}
